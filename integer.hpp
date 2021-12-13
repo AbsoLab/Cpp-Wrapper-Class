dc16e4b03e248c5110ebd16c842b783f3d6aa705
Hertz <kljj0650@naver.com>
2021-12-13 10:33:01 +0900

    구현할 기능 작성

---
+++ integer.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+// int의 wrapper class
+/*
+    구현 할 기능
+    1. toString()
+    2. x진법으로 출력: toString(2) 2진법
+    3. 문자열을 int로: parseInt(), parseInt(2) 2진법
+    4. 연산자 오버로딩
+    5. int로 형 변환이 자유
+    6. cout, printf로 출력 가능
+*/
+
+class MyInteger {
+
+private:
+    int integer;
+
+public:
+    MyInteger();
+
+};
